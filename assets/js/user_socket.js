56315a5d8475269d7b48e636aba16b6844005a7d
KubaLobodzinski <quz020@gmail.com>
2022-03-24 19:05:27 +0100

    Final commit, user can login via github, create topics, add comments to them and brwose through different comments.

--- assets/js/user_socket.js
+++ assets/js/user_socket.js
@@ -56,27 +56,52 @@ socket.connect()
 const createSocket = (topicId) => {
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { renderComments(resp.comments) })
+    .receive("ok", resp => { 
+      console.log(resp);
+      renderComments(resp.comments); 
+    })
     .receive("error", resp => { console.log("Unable to join", resp) })
 
+    channel.on(`comments:${topicId}:new`, renderComment);
+
+    if (document.querySelector("button")){
     document.querySelector('button').addEventListener('click', () => {
       const content = document.querySelector('textarea').value;
 
       channel.push('comment:add', { content: content });
-    })
+    });
+  }
 }
 
 
 function renderComments(comments) {
   const renderedComments = comments.map(comment =>{
-    return `
-      <li class="collection-item">
-      ${comment.content}
-      </li>
-    `;
+    return commentTemplate(comment);
   });
 
   document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
+
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
+
+  document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+function commentTemplate(comment){
+  let email = 'Anonymous'
+  if(comment.user) {
+    email = comment.user.email;
+  }
+  return `
+      <li class="collection-item">
+        ${comment.content}
+        <div class="secondary-content">
+          ${email}
+        </div>
+      </li>
+    `;
 }
 
 window.createSocket = createSocket;
--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -12,9 +12,9 @@ defmodule DiscussWeb.CommentsChannel do
       topic_id = String.to_integer(topic_id)
       topic = Topic
         |> Repo.get(topic_id)
-        |> Repo.preload(:comments)
+        |> Repo.preload(comments: [:user])
 
-      {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
+      {:ok, %{comments: topic.comments  }, assign(socket, :topic, topic)}
     else
       {:error, %{reason: "unauthorized"}}
     end
@@ -23,13 +23,16 @@ defmodule DiscussWeb.CommentsChannel do
   @impl true
   def handle_in(_name, %{"content" => content}, socket) do
     topic = socket.assigns.topic
+    user_id = socket.assigns.user_id
 
     changeset = topic
-    |> build_assoc(:comments)
+    |> build_assoc(:comments, user_id: user_id)
     |> Comment.changeset(%{content: content})
 
     case Repo.insert(changeset) do
-      {:ok, _comment} ->
+      {:ok, comment} ->
+        comment = Repo.preload(comment, :user)
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new", %{comment: comment})
         {:reply, :ok, socket}
       {:error, _reason} ->
         {:reply, {:error, %{errors: changeset}}, socket}
